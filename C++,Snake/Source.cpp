6c41a25b149e9eb9f84a52864d9e0086b17f8dd9
BiggieFudge <73502334+BiggieFudge@users.noreply.github.com>
2021-07-31 20:50:24 +0300

    Update Source.cpp

--- C++,Snake/Source.cpp
+++ C++,Snake/Source.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
 #include <windows.h>
 #include <synchapi.h>
 #include <stdlib.h>     
 #include <conio.h>
 #include <time.h> 
+#include <io.h>
+#include <fcntl.h>
 using namespace std;
 
+int columns, rows;  // Size of the console
 
 
 
@@ -27,6 +30,19 @@ snake Snake;
 apple Apple;
 
 
+
+
+void HideConsoleCursor(){  
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = false; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+
 bool SearchArray(int i, int j) {
 	int x = 0;
 
@@ -38,12 +54,36 @@ bool SearchArray(int i, int j) {
 	return false;
 }
 
-bool gameover(int rows,int columns) {
+void GenerateApple(int rows, int columns) {
+	Apple.x = (rand() % (rows - 2));
+	Apple.y = (rand() % (columns - 2));
+}
+
+void Initialize(int rows,int columns) {
+
+	
+	Snake.x = (int*)calloc(1, sizeof(int));
+	Snake.y = (int*)calloc(1, sizeof(int));
+	
+	Snake.x[0] = (rand() % (rows - 3)) + 1;
+	Snake.y[0] = (rand() % (columns - 3)) + 1;
+	GenerateApple(rows, columns);
+	Snake.direction = 0;
+}
+
+bool gameover() {
+	char PlayerChoice;
 	if (Snake.x[0] == -1 || Snake.x[0] == rows -2 || Snake.y[0] == -1 || Snake.y[0] == columns-2/*Check if out of bounds*/ ||
 		SearchArray( Snake.y[0] , Snake.x[0]) /*Check if hits itself*/) {
 		system("CLS");
-		cout << "Game Over";
-		return false;
+		cout << "Game Over\nyou scored:" << Snake.size <<"\nEnter any char to continue (0 to exit)...\n";
+		cin >> PlayerChoice;
+		if(PlayerChoice =='0')
+			return false;
+		else {
+
+			Initialize(rows, columns);
+		}
 	}
 	
 	return true;
@@ -67,10 +107,7 @@ void ForwardTheLast() {
 	Snake.y[0] = x;
 }
 
-void GenerateApple(int rows,int columns){
-	Apple.x = (rand() % (rows - 2)) ;
-	Apple.y = (rand() % (columns - 2)) ;
-}
+
 
 bool ChangeDirection() {
 	SHORT UpkeyState = GetKeyState(VK_UP/*(UP)*/);
@@ -98,8 +135,9 @@ bool ChangeDirection() {
 }
 
 
-bool move(int rows,int columns) {
+bool move() {
 
+	int speed = 15;
 	if ((Snake.direction==1 && 
 		Snake.y[0] == Apple.y && Snake.x[0]-1 == Apple.x) ||
 		(Snake.direction == 2 &&
@@ -181,16 +219,19 @@ bool move(int rows,int columns) {
 		
 	}
 	ChangeDirection();
-	Sleep(100);
+	
+	Sleep(speed);
+	
 	ChangeDirection();
-	return gameover(rows, columns);
+	
+	return gameover();
 }
 
 
-bool play(int rows,int columns) {
+bool play() {
 	system("CLS");
 	for (int i = 0; i < columns; i++) {
-		cout << "!";
+		cout << "=";
 	}
 
 
@@ -214,38 +255,40 @@ bool play(int rows,int columns) {
 	}
 
 	for (int i = 0; i < columns; i++) {
-		cout << "!";
+		cout << "=";
 	}
 
 	ChangeDirection();
 
-	return move(rows,columns);
+	return move();
 	
 
 	
 }
 
 
-int main(int argc, char *argv[]) {
+int main() {
+
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::cout.flush();
+	SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY); //Set the console color
+	
+	
+	HideConsoleCursor(); //Hides the curses
 	srand(time(NULL));
 	//Check console size
 	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	int columns, rows;
 
 	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
 	columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
 	rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 	//End Check
-
-	Snake.x = (int*) calloc(1,sizeof(int));
-	Snake.y = (int*)calloc(1,sizeof(int));
-	Snake.x[0] = (rand() % (rows-2)) + 1;
-	Snake.y[0] = (rand() % (columns-2)) + 1;
-	GenerateApple(rows,columns);
+	Initialize(rows, columns);
+	
 	do {
 		ChangeDirection();
 		
 
-	} while (play(rows, columns));
+	} while (play());
 
 }
